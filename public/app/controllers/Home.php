e688429a3c016bdbadb9c4c1ee8433d86d882e0e
Johan Havenga <johan.havenga@gmail.com>
2018-08-22 14:26:03 +0200

    Initial commit with all CI code 3.1.9

---
+++ public/app/controllers/Home.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends Frontend_Controller {
+    
+    function __construct()
+    {
+        parent::__construct();
+    }
+         
+    public function index()
+    {     
+        $this->load->view($this->header_url, $this->data_to_header);
+        $this->load->view('home', $this->data_to_view);
+        $this->load->view($this->footer_url, $this->data_to_footer);
+    }
+
+    public function my_404() 
+    {
+        $this->data_to_header['title']="Page not found";
+
+        $this->load->view($this->header_url, $this->data_to_header);
+        $this->load->view('404', $this->data_to_view);
+        $this->load->view($this->footer_url, $this->data_to_footer);
+    }
+    
+}
---
+++ public/app/core/MY_Controller.php
@@ -0,0 +1,32 @@
+<?php
+// core/MY_Controller.php
+/**
+ * Base Controller
+ *
+ */
+class MY_Controller extends CI_Controller {
+
+    function __construct()
+    {
+        parent::__construct();
+        // Load any front-end only dependencies
+    }
+
+}
+
+
+class Frontend_Controller extends MY_Controller {
+
+    public $data_to_header=[];
+    public $data_to_view=[];
+    public $data_to_footer=[];
+
+    public $header_url='templates/header';
+    public $footer_url='templates/footer';
+
+    function __construct()
+    {
+        parent::__construct();                
+    }    
+
+}
